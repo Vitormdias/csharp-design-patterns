2e1f64bb6081d1c747ba4957d53b818bca3466c9
vitormdias <vitormd97@gmail.com>
2018-01-06 11:53:04 -0200

    Continua pattern Strategy

--- Estudo/DesignPatterns/Orcamento.cs
+++ Estudo/DesignPatterns/Orcamento.cs
@@ -7,10 +7,17 @@ namespace CursoDesignPatterns.StrategyPattern
     public class Orcamento
     {
         public double Valor { get; private set; }
+        public IList<Item> Itens { get; private set; }
 
         public Orcamento (double valor)
         {
-            this.Valor = valor;
+            Valor = valor;
+            Itens = new List<Item>();
+        }
+
+        public void AdicionaItem (Item item)
+        {
+            Itens.Add(item);
         }
     }
 }
